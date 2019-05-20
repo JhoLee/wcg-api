0371ec95927f42a6a87a21bef83ef4511472307e
Joo-Ho Lee <jooho_lee@outlook.kr>
2019-05-20 15:35:49 +0900

    ENH: Build WCG

--- wcg.py
+++ wcg.py
@@ -4,6 +4,7 @@ from random import random
 from PIL import Image
 import numpy as np
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from matplotlib import pyplot as plt
 
 from palettable.colorbrewer.qualitative import *
 
@@ -12,55 +13,38 @@ def color_func(word, font_size, position, orientation, random_state=None, **kwar
     return tuple(Dark2_8.colors[random.randint(0, 7)])
 
 
-current_datetime = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+class WCG:
+    def __init__(self, title, data, font, mask_image):
+        self.title: str = title
+        self.data: list = data
+        self.font: str = font
 
-image = 'apple1'
-icon_path = "./img/" + image + ".png"
-font = "BMHANNA_11yrs_otf"
-font_path = "./otf/" + font + ".otf"
-data = "sample4"
-data_path = "./data/" + data + ".txt"
-background_color = "white"
+        icon = Image.open(icon_path).comvert("RGBA")
+        self.mask_image = Image.new("RGB", icon.size, (255, 255, 255))
+        self.mask_image.paste(icon, icon)
+        self.mask_image = mask_image.array(mask_image)
 
-save_path = "./result/" + image + "_" + font + "_" + data + "_" + background_color + "_" + current_datetime + ".png"
-with open(data_path) as f:
-    message = f.read()
-    print(message)
+        self.save_path = "./images/wordcloud/{title}_{font}.png".format(
+            title=self.title, font=self.font
+        )
+        self.result = ""
+        self.background_color = "white"
 
-icon = Image.open(icon_path).convert("RGBA")
-mask = Image.new("RGB", icon.size, (255, 255, 255))
-mask.paste(icon, icon)
-mask = np.array(mask)
+    def generate(self):
+        self.result = WordCloud(font_path=self.font, background_color=self.background_color, mask=self.mask_image,
+                                stopwords=STOPWORDS,
+                                max_font_size=300)
+        coloring = np.array(Image.open(self.mask_image))
+        image_colors = ImageColorGenerator(coloring)
+        image_colors.default_color = [0.6, 0.6, 0.6]
 
-print(type(STOPWORDS))
-STOPWORDS.add('있습니다')
-STOPWORDS.add('또는')
-STOPWORDS.add('귀하의')
-STOPWORDS.add('귀하는')
-STOPWORDS.add('귀하가')
-STOPWORDS.add('다른')
-STOPWORDS.add('있다')
-STOPWORDS.add('있는')
-STOPWORDS.add('jpg')
-STOPWORDS.add('한다')
-STOPWORDS.add('이러한')
-STOPWORDS.add('모든')
-STOPWORDS.add('경우')
-STOPWORDS.add('합니다')
-STOPWORDS.add('서비스')
+        self.result.generate_from_text(self.data)
+        self.result.recolor(None, image_colors)
+        self.result.to_file(self.save_path)
 
+        print('Result saved to \"{}"'.format(self.save_path))
 
-
-wc = WordCloud(font_path=font_path, background_color=background_color, mask=mask, stopwords=STOPWORDS,
-               max_font_size=300)
-
-coloring = np.array(Image.open(icon_path))
-image_colors = ImageColorGenerator(coloring)
-image_colors.default_color = [0.6, 0.6, 0.6]
-
-wc.generate_from_text(message)
-wc.recolor(None, image_colors)
-wc.to_file(save_path)
-print('Result saved to \"{}"'.format(save_path))
-
-
+        plt.imshow(self.result, interpolation='bilinear')
+        plt.axis("off")
+        plt.figure()
+        plt.show()
